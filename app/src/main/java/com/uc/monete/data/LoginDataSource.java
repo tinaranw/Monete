3076065669128a7121403e5aac50a25526602f56
michellealexandra <62871142+michellealexandra@users.noreply.github.com>
2020-04-23 20:26:21 +0700

    tes

--- app/src/main/java/com/uc/monete/data/LoginDataSource.java
+++ app/src/main/java/com/uc/monete/data/LoginDataSource.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class LoginDataSource {
 
     public Result<LoggedInUser> login(String username, String password) {
-
+//makan nasi bebek sama tinara
         try {
             // TODO: handle loggedInUser authentication
             LoggedInUser fakeUser =
