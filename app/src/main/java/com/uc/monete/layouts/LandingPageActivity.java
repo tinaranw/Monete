d9ddd1223d795bcde308f1ee6b4a0ac18af2a508
michellealexandra <62871142+michellealexandra@users.noreply.github.com>
2020-04-29 11:27:25 +0700

    json and alert

--- app/src/main/java/com/uc/monete/layouts/LandingPageActivity.java
+++ app/src/main/java/com/uc/monete/layouts/LandingPageActivity.java
@@ -1,5 +1,35 @@
 package com.uc.monete.layouts;
 
-public class LandingPageActivity {
 
-}
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.uc.monete.R;
+
+public class LandingPageActivity extends AppCompatActivity {
+
+        LottieAnimationView wave;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            wave = findViewById(R.id.wavee);
+            wave.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    wave.playAnimation();
+                    Toast.makeText(LandingPageActivity.this, "Hi there, I'm Monete!", Toast.LENGTH_SHORT).show();
+                    //---- Your code here------
+                }
+            });
+
+
+        }
+    }
+
