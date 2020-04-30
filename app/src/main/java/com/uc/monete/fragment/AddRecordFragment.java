58a7c03520bf54f2ad18a3481bb9f5fc31911d0c
Tinara Nathania Wiryonoputro <tinaranathania@gmail.com>
2020-04-30 18:22:56 +0700

    Masih masalah spinner dalemnya fragment
    tasukete kudasai :(

--- app/src/main/java/com/uc/monete/fragment/AddRecordFragment.java
+++ app/src/main/java/com/uc/monete/fragment/AddRecordFragment.java
@@ -7,61 +7,33 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 
 import com.uc.monete.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link AddRecordFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link AddRecordFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class AddRecordFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class AddRecordFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
-
+    Spinner spinnertag, spinnertype;
     public AddRecordFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AddRecordFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AddRecordFragment newInstance(String param1, String param2) {
-        AddRecordFragment fragment = new AddRecordFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
@@ -74,6 +46,50 @@ public class AddRecordFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        Button btn_addrecord = view.findViewById(R.id.fr_addrecord);
+        Button btn_discardrecord = view.findViewById(R.id.fr_discardrecord);
+
+        spinnertag = view.findViewById(R.id.spinner_tags);
+        spinnertype = view.findViewById(R.id.spinner_inputtype);
+
+        ArrayAdapter<CharSequence> adapterTag  = ArrayAdapter.createFromResource(view.getContext(), R.array.tags, android.R.layout.simple_spinner_item);
+        adapterTag.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnertag.setAdapter(adapterTag);
+//        spinnertag.setOnItemClickListener(view.getContext());
+
+        btn_addrecord.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new HistoryFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.frame_main, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        btn_discardrecord.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new HomeFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.frame_main, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
