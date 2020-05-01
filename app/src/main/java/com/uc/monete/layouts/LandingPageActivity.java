0b7edd9eb2ab73055ae709b5959c574bf30ec613
Tinara Nathania Wiryonoputro <tinaranathania@gmail.com>
2020-05-01 16:41:23 +0700

    doneee

--- app/src/main/java/com/uc/monete/layouts/LandingPageActivity.java
+++ app/src/main/java/com/uc/monete/layouts/LandingPageActivity.java
@@ -23,7 +23,7 @@ public class LandingPageActivity extends AppCompatActivity {
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+            setContentView(R.layout.activity_landingpage);
 
             wave = findViewById(R.id.wave);
 //            wave.setOnClickListener(new View.OnClickListener() {
